205fbe3e463002beb8fbd90b0a562475afcc2ffb
Ales Teska <ales.teska@teskalabs.com>
2022-10-16 14:29:02 +0200

    Update seacat-client.py

--- seacat-client.py
+++ seacat-client.py
@@ -5,7 +5,7 @@ import seacatcpki
 
 
 def main():
-	SeaCat = seacatcpki.SeaCat("SeaCat Demo Client", "https://masterdcpoc.seacat.io/seacat")
+	SeaCat = seacatcpki.SeaCat("SeaCat Demo Client", "https://localhost.seacat.io/seacat")
 	print("Identity: {}".format(SeaCat.Identity.identity()))
 	while True:
 		time.sleep(500)
